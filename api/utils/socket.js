51b169137f0b9df8494465eef987262fb74a4e1b
RealNiiclas <43724061+RealNiiclas@users.noreply.github.com>
2022-02-04 18:36:34 +0100

    Fixed a bug to prevent unauthorized chat messages from being sent

--- api/utils/socket.js
+++ api/utils/socket.js
@@ -1,5 +1,13 @@
 function handleSocket(io, socket) {
-  socket.on("message", (message) => io.sockets.emit("message", { message, from: socket.request.session.name }));
+  if (!socket.request.session.name) {
+    socket.emit("unauthed");
+    socket.disconnect();
+    return;
+  }
+
+  socket.on("message", (message) => 
+    io.sockets.emit("message", { message, from: socket.request.session.name }));
+  
   socket.on("messageTo", (message, name) => {
     const receiver = Array.from(io.sockets.sockets.values())
       .find((sock) => sock.request.session.name === name);
@@ -8,10 +16,6 @@ function handleSocket(io, socket) {
     receiver.emit("message", { message, from: `Von ${socket.request.session.name}` });
     socket.emit("message", { message, from: `An ${receiver.request.session.name}` })
   });
-  if (!socket.request.session.name) {
-    socket.emit("unauthed");
-    socket.disconnect();
-  }
 }
 
 function disconnectSocket(io, id) {
